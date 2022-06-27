4dff903568350e1157fcfd6b700e1e1b0e56d9b5
武秦乾 <wuqinqian@xiaoyezi.com>
2022-06-27 18:51:05 +0800

    uppercaseKey.ts

--- src/lesson-1/index.vue
+++ src/lesson-1/index.vue
@@ -2,9 +2,12 @@
 
 <script setup lang="ts">
 import * as THREE from 'three';
+import State from 'three/examples/jsm/libs/stats.module.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 const w = window.innerWidth;
 const h = window.innerHeight;
-
+const state = State();
 // scene
 const scene = new THREE.Scene();
 
@@ -12,36 +15,66 @@ const scene = new THREE.Scene();
 const axes = new THREE.AxesHelper(2);
 scene.add(axes);
 
-// const cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial();
-const cube = new THREE.Mesh(geometry, material);
-cube.position.set(-1, -1, -1);
-cube.rotateX((45 / 180) * Math.PI);
-cube.rotateY((45 / 180) * Math.PI);
-scene.add(cube);
+// 光源
 
-// light
-const light = new THREE.AmbientLight();
-scene.add(light);
+// object
+const carGroup = new THREE.Group();
+
+const bodyGroup = new THREE.Group();
+
+const bodyCube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 2, 0.5),
+    new THREE.MeshNormalMaterial()
+);
+const bodyCube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.5, 0.5, 0.5),
+    new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+    })
+);
+bodyCube2.position.z = 0.5;
+
+bodyGroup.add(bodyCube1);
+bodyGroup.add(bodyCube2);
+
+const wheelGroup = new THREE.Group();
+const wheel1 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.1, 0.7, 0.7),
+    new THREE.MeshBasicMaterial({
+        color: 0x0000ff,
+    })
+);
+wheel1.position.set(-0.7, 0.5, 0);
+wheelGroup.add(wheel1);
+
+carGroup.add(bodyGroup);
+carGroup.add(wheelGroup);
+scene.add(carGroup);
 
 // camera
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 100);
 camera.position.set(0, 0, 5);
 camera.lookAt(0, 0, 0);
 
+// Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
 document.body.append(renderer.domElement);
+document.body.append(state.dom);
+
+// orbit controls
+const orbitControls = new OrbitControls(camera, renderer.domElement);
 
 const clock = new THREE.Clock();
 tick();
 function tick() {
-    const time = clock.getElapsedTime() * 1.5;
-    cube.rotation.z = time;
-    cube.position.x = Math.sin(time * 10) * 2;
-    cube.position.y = Math.cos(time) * 2;
-    requestAnimationFrame(tick);
+    const time = clock.getElapsedTime();
+    wheel1.rotation.x = time;
+    carGroup.position.y = Math.sin(time) * 2;
     renderer.render(scene, camera);
+
+    state.update();
+    orbitControls.update();
+    requestAnimationFrame(tick);
 }
 </script>
