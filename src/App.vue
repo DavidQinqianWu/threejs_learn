2a1fff67db01c31d96d791825667c10cb7f54e78
武秦乾 <wuqinqian@xiaoyezi.com>
2022-06-28 14:51:37 +0800

    添加阴影

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
     <!-- <Lesson1 /> -->
-    <Lesson2 />
+    <!-- <Lesson2 /> -->
+    <Lesson3 />
 </template>
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Lesson1 from './lesson-1/index.vue';
-import Lesson2 from './lesson-2/index.vue';
+// import Lesson1 from './lesson-1/index.vue';
+// import Lesson2 from './lesson-2/index.vue';
+import Lesson3 from './lesson-3/index.vue';
 </script>
 
 <style>
